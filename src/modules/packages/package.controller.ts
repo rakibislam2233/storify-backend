a851b534cb60c6fe81212bd56383c726cd79d9c0
Md Rakib Ali <rakib2020.tkg@gmail.com>
2026-03-03 13:44:22 +0600

    feat: add getPackageById, updatedPackage, and deletedPackage functions to package controller

--- src/modules/packages/package.controller.ts
+++ src/modules/packages/package.controller.ts
@@ -5,6 +5,7 @@ import pick from '../../utils/pick.utils';
 import sendResponse from '../../utils/sendResponse';
 import { PackageService } from './package.service';
 
+// -- Create Package --
 const createPackage = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
   const result = await PackageService.createPackage(payload);
@@ -16,6 +17,7 @@ const createPackage = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+// -- Get All Packages --
 const getAllPackages = catchAsync(async (req: Request, res: Response) => {
   const filters = pick(req.query, ['searchTerm']);
   const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder']);
@@ -28,3 +30,48 @@ const getAllPackages = catchAsync(async (req: Request, res: Response) => {
     data: result.data,
   });
 });
+
+// -- Get Package By Id --
+const getPackageById = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const result = await PackageService.getPackageById(id);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Package retrieved successfully',
+    data: result,
+  });
+});
+
+// -- Update Package --
+const updatedPackage = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const payload = req.body;
+  const result = await PackageService.updatedPackage(id, payload);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Package updated successfully',
+    data: result,
+  });
+});
+
+// -- Delete Package --
+const deletedPackage = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  await PackageService.deletedPackage(id);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Package deleted successfully',
+  });
+});
+
+// -- Export Package Controller --
+export const PackageController = {
+  createPackage,
+  getAllPackages,
+  getPackageById,
+  updatedPackage,
+  deletedPackage,
+};
