fac99c77aeaf169e560fcda6afe1ece22f29fc7a
Md Rakib Ali <rakib2020.tkg@gmail.com>
2026-03-03 13:06:15 +0600

    feat: update User schema to include activePackage relation and add Package model with interface

---
+++ prisma/migrations/20260303070048_add_new_schema_and_update_user_schema/migration.sql
@@ -0,0 +1,52 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "profileImage" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "lastLoginAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "activePackageId" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "packages" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "maxFolders" INTEGER NOT NULL,
+    "maxNestingLevel" INTEGER NOT NULL,
+    "allowedFileTypes" TEXT[],
+    "maxFileSize" DOUBLE PRECISION NOT NULL,
+    "totalFileLimit" INTEGER NOT NULL,
+    "filesPerFolder" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "packages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "packages_name_key" ON "packages"("name");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_activePackageId_fkey" FOREIGN KEY ("activePackageId") REFERENCES "packages"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/modules/packages/packages.interface.ts
@@ -0,0 +1,10 @@
+// -- Create Package Interface --
+export interface ICreatePackage {
+  name: string;
+  maxFolders: number;
+  maxNestingLevel: number;
+  allowedFileTypes: string[];
+  maxFileSize: number;
+  totalFileLimit: number;
+  filesPerFolder: number;
+}
